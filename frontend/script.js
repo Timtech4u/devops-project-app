2fd2f9f2802521a9a902c7d0dfc1eadfc0938e1e
Mariam <129985082+MariammBello@users.noreply.github.com>
2024-07-29 15:50:28 +0100

    Update script.jswith new backend/API URL

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.getElementById('increment-btn').addEventListener('click', function() {
-    fetch('http://localhost:5555/counter/increment', { method: 'POST' })
+    fetch('http://18.130.110.234:5555/counter/increment', { method: 'POST' })
         .then(response => response.json())
         .then(data => {
             document.getElementById('counter').innerText = data.counter;
@@ -7,7 +7,7 @@ document.getElementById('increment-btn').addEventListener('click', function() {
 });
 
 function updateCounter() {
-    fetch('http://localhost:5555/counter')
+    fetch('http://18.130.110.234:5555/counter')
         .then(response => response.json())
         .then(data => {
             console.log(data);
